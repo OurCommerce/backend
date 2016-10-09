0d76bac6a910c03c80fcef54d91ec950ad0f414a
arpit o.O <arpitremarkable@gmail.com>
2016-10-09 21:19:36 +0530

    Initiated multichat

--- base/settings.py
+++ base/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ] + [
+    'base',
+    'django_extensions',
     'channels',
+    'yap',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "asgiref.inmemory.ChannelLayer",
+        "ROUTING": "base.routing.channel_routing",
+    },
+}
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- base/urls.py
+++ base/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
+from yap.views import index
+
 
 urlpatterns = [
+    url(r'^$', index),
+    url(r'^accounts/login/$', login),
+    url(r'^accounts/logout/$', logout),
     url(r'^admin/', admin.site.urls),
 ]
